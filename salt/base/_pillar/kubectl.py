0a3bd59eb2b9d27219aeb38c1fb27f6e097d2909
stanislaw_jakiel <stanislaw.dev@gmail.com>
2018-10-29 18:56:55 +0100

    Proper key wrap of kubectl pillar ext data

--- salt/base/_pillar/kubectl.py
+++ salt/base/_pillar/kubectl.py
@@ -1,6 +1,7 @@
 import logging
 
 import salt.utils.yaml
+from salt.defaults import DEFAULT_TARGET_DELIM
 
 log = logging.getLogger(__name__)
 
@@ -12,23 +13,28 @@ def __virtual__():
 
 
 def ext_pillar(minion_id, pillar, *args, **kwargs):
-    def merge(input_dict, output_dict):
-        for e in input_dict:
-            output_dict = salt.utils.dictupdate.merge(
-                output_dict,
-                e,
-                strategy='smart',
-                merge_lists=True
-            )
-        return output_dict
-
     ret = {}
+
+    def wrap(keys, val):
+        if len(keys) > 1:
+            return {keys[0]: wrap(keys[1:], val)}
+        elif len(keys) == 1:
+            return {keys[0]: val}
+        else:
+            return {}
+
     config_path = kwargs['config']
     queries = kwargs['queries']
     for query_conf in queries:
         command = "kubectl get {} {} -o yaml".format(query_conf['type'], query_conf['name'])
         output = __salt__['cmd.run_stdout'](command, python_shell=True, env={'KUBECONFIG': config_path})
-        data = {query_conf['key']: salt.utils.yaml.safe_load(output)}
-        merge(data, ret)
-
+        if output:
+            key_list = query_conf['key'].split(DEFAULT_TARGET_DELIM)
+            data = wrap(key_list, salt.utils.yaml.safe_load(output))
+            ret = salt.utils.dictupdate.merge(
+                ret,
+                data,
+                strategy='smart',
+                merge_lists=True
+            )
     return ret
