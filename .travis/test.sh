81fa822a28dc014b151a5f6d009b3d6ca71c9577
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-02-06 09:53:17 +0100

    missing 'include'

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source .travis/common.sh
+
 while sleep 9m; do echo "=====[ $SECONDS seconds still running ]====="; done &
 docker run --privileged "$DOCKER_USERNAME/envoy-dry-test-$DOCKER_IMAGE:$TAG"
 kill %1
