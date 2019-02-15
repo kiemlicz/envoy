01ade9095ef18ebb65fc06990da62714aaf1613d
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-02-15 14:07:39 +0100

    WIP kubernetes client as SDB module, removing kubectl as pillar module

---
+++ salt/base/_utils/k8s.py
@@ -0,0 +1,80 @@
+import logging
+import os
+
+from salt.exceptions import CommandExecutionError
+
+try:
+    from kubernetes import client, config
+    from kubernetes.client.rest import ApiException
+
+    HAS_K8S = True
+except ImportError:
+    HAS_K8S = False
+
+
+log = logging.getLogger(__name__)
+
+
+class K8sClient(object):
+    def __init__(self, profile=None, **kwargs):
+        if not HAS_K8S:
+            raise CommandExecutionError('(unable to import kubernetes, module most likely not installed)')
+
+        self.active_namespace = "default"
+
+        if "KUBERNETES_SERVICE_HOST" in os.environ:
+            config.load_incluster_config()
+            with open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r") as f:
+                self.active_namespace = f.read()
+        else:
+            try:
+                # todo handle opts from profile
+                config.load_kube_config()
+            except FileNotFoundError as e:
+                raise CommandExecutionError("unable to create kubernetes config: {}".format(e))
+
+        self.client_apps_v1_api = client.AppsV1Api()
+        self.client_core_v1_api = client.CoreV1Api()
+
+    def read(self, kind, name=None, namespace=None):
+        if not namespace:
+            namespace = self.active_namespace
+        method = "read_namespaced_{}".format(kind)
+        try:
+            client = self._client(kind)
+            result = getattr(client, method)(name=name, namespace=namespace)
+            return result.to_dict()
+        except AttributeError as e:
+            log.exception(e)
+            raise CommandExecutionError("Method {} not found".format(method))
+        except ApiException as e:
+            log.error("unable to fetch: {}".format(kind))
+            log.exception(e)
+            return None
+
+    def list(self, kind, label_selector=None, namespace=None):
+        if not namespace:
+            namespace = self.active_namespace
+        method = "list_namespaced_{}".format(kind)
+        try:
+            client = self._client(kind)
+            result = getattr(client, method)(namespace=namespace, label_selector=label_selector)
+            return result.to_dict()
+        except AttributeError as e:
+            log.exception(e)
+            raise CommandExecutionError("Method {} not found".format(method))
+        except ApiException as e:
+            log.error("unable to fetch: {}".format(kind))
+            log.exception(e)
+            return None
+
+    def _client(self, kind):
+        if kind == "pod":
+            return self.client_core_v1_api
+        else:
+            return self.client_apps_v1_api
+
+
+def k8s_client(profile=None, **kwargs):
+    log.info("Creating K8s client for profile: {}".format(profile))
+    return K8sClient(profile, **kwargs)
