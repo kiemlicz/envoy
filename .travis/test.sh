7c5dc8d50648295c1f10b44927b0bb34f246fd0b
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-03-02 19:30:14 +0100

    Proper exit code in tests

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,8 +3,7 @@
 source .travis/common.sh
 
 while sleep 9m; do echo "=====[ $SECONDS seconds still running ]====="; done &
-# adding "-t" for test stdout
-docker run -t --privileged "$DOCKER_USERNAME/envoy-dry-test-$DOCKER_IMAGE:$TAG"
+docker run --privileged "$DOCKER_USERNAME/envoy-dry-test-$DOCKER_IMAGE:$TAG"
 result=$?
 kill %1
 # in order to return proper exit code instead of always 0 (of kill command)
