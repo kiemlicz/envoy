467497ec24ca4cddf53f25e1f3a2a40e176454c1
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-03-02 17:01:13 +0100

    Switching to python3 by default

--- salt/base/_sdb/kubectl.py
+++ salt/base/_sdb/kubectl.py
@@ -42,7 +42,7 @@ def _query_string_to_dict(qs):
     query_dict = parse_qs(qs)
 
     # unwrap values as by default arguments are parsed to lists { 'k': ['v']}
-    for k, v in query_dict.iteritems():
+    for k, v in query_dict.items():
         if isinstance(v, list) and len(v) == 1:
             query_dict[k] = v[0]
 
