c90503fe6c885a094528092743de81b809e33899
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-03-15 18:46:20 +0100

    Masterless tests merged with envoy

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -4,28 +4,33 @@ source .travis/common.sh
 
 docker_update
 
-docker build \
-    --build-arg=salt_ver=$SALT_VER \
-    --target salt-minion \
-    -t "envoy-minion-$DOCKER_IMAGE:$TAG" \
-    -f .travis/"$DOCKER_IMAGE"/Dockerfile .
+case "$1" in
+dry)
+    docker build \
+        --build-arg=salt_ver=$SALT_VER \
+        --target salt-minion \
+        -t "envoy-minion-$DOCKER_IMAGE:$TAG" \
+        -f .travis/"$DOCKER_IMAGE"/Dockerfile .
 
-docker build \
-    --build-arg=salt_ver=$SALT_VER \
-    --target salt-master \
-    -t "envoy-master-$DOCKER_IMAGE:$TAG" \
-    -f .travis/"$DOCKER_IMAGE"/Dockerfile .
+    docker build \
+        --build-arg=salt_ver=$SALT_VER \
+        --target salt-master \
+        -t "envoy-master-$DOCKER_IMAGE:$TAG" \
+        -f .travis/"$DOCKER_IMAGE"/Dockerfile .
 
-docker build \
-    --build-arg=salt_ver=$SALT_VER \
-    --target dry-test \
-    -t "envoy-dry-test-$DOCKER_IMAGE:$TAG" \
-    -f .travis/"$DOCKER_IMAGE"/Dockerfile .
-
-docker build \
-    --build-arg=salt_ver=$SALT_VER \
-    --build-arg=log_level="${LOG_LEVEL-info}" \
-    --build-arg=saltenv="$SALTENV" \
-    --target masterless-test \
-    -t "$DOCKER_USERNAME/masterless-test-$DOCKER_IMAGE:$TAG" \
-    -f .travis/"$DOCKER_IMAGE"/Dockerfile .
+    docker build \
+        --build-arg=salt_ver=$SALT_VER \
+        --target dry-test \
+        -t "envoy-dry-test-$DOCKER_IMAGE:$TAG" \
+        -f .travis/"$DOCKER_IMAGE"/Dockerfile .
+    ;;
+masterless)
+    docker build \
+        --build-arg=salt_ver=$SALT_VER \
+        --build-arg=log_level="${LOG_LEVEL-info}" \
+        --build-arg=saltenv="$SALTENV" \
+        --target masterless-test \
+        -t "masterless-test-$DOCKER_IMAGE:$TAG" \
+        -f .travis/"$DOCKER_IMAGE"/Dockerfile .
+    ;;
+esac
--- .travis/test.sh
+++ .travis/test.sh
@@ -2,9 +2,31 @@
 
 source .travis/common.sh
 
-while sleep 9m; do echo "=====[ $SECONDS seconds still running ]====="; done &
-docker run --privileged "envoy-dry-test-$DOCKER_IMAGE:$TAG"
-result=$?
-kill %1
-# in order to return proper exit code instead of always 0 (of kill command)
-exit $result
+case "$1" in
+dry)
+    while sleep 9m; do echo "=====[ $SECONDS seconds still running ]====="; done &
+    docker run --privileged "envoy-dry-test-$DOCKER_IMAGE:$TAG"
+    result=$?
+    kill %1
+    # in order to return proper exit code instead of always 0 (of kill command)
+    exit $result
+    ;;
+masterless)
+    # privileged mode is necessary for e.g. setting: net.ipv4.ip_forward or running docker in docker
+    name="ambassador-salt-masterless-run-$TRAVIS_JOB_NUMBER"
+    while sleep 9m; do echo "=====[ $SECONDS seconds still running ]====="; done &
+    docker run --name $name --hostname "$CONTEXT-host" --privileged "masterless-test-$DOCKER_IMAGE:$TAG" 2>&1 | tee output
+    exit_code=$?
+    kill %1
+    if [[ "$exit_code" != 0 ]]; then
+        echo "found failures"
+        exit $exit_code
+    fi
+    result=$(awk '/^Failed:/ {if($2 != "0") print "fail"}' output)
+    if [[ "$result" == "fail" ]]; then
+        echo "found failures"
+        exit 3
+    fi
+    ;;
+esac
+
