1cfe47a5b1f3de307e34c0c3c06260af386b7ada
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-03-20 22:34:16 +0100

    Removing functions used only in ambassador

--- .travis/common.sh
+++ .travis/common.sh
@@ -31,30 +31,6 @@ docker_push() {
     docker push "$1"
 }
 
-minikube_ready() {
-    echo "Waiting for nodes..."
-    kubectl get nodes
-    kubectl wait nodes/minikube --for condition=ready
-    echo "minikube setup complete"
-}
-
-salt_install() {
-    sudo apt-get update && sudo apt-get install -y curl
-    sudo mkdir -p /etc/salt/minion.d/
-    sudo cp ${1-".travis/config/masterless.conf"} /etc/salt/minion.d/
-    sudo ln -s $TRAVIS_BUILD_DIR/envoy/salt /srv/salt
-    sudo ln -s $TRAVIS_BUILD_DIR/.travis/pillar /srv/pillar
-    curl -o /tmp/bootstrap-salt.sh -L https://bootstrap.saltstack.com
-    sudo sh /tmp/bootstrap-salt.sh -n stable
-}
-
-minikube_install() {
-    sudo salt-call --local state.apply kubernetes.client saltenv=server
-    sudo salt-call --local state.apply kubernetes.minikube saltenv=server
-    sudo salt-call --local state.apply kubernetes.helm saltenv=server
-    minikube_ready
-}
-
 still_running() {
     minutes=0
     limit=60
