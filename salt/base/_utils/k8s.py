57f0204d34d5dde8092841d9a4a791325d75a1b4
stanislaw_jakiel <stanislaw.dev@gmail.com>
2019-04-01 21:12:22 +0200

    Kubernetes client refactor

--- salt/base/_utils/k8s.py
+++ salt/base/_utils/k8s.py
@@ -38,29 +38,22 @@ class K8sClient(object):
         self.client_core_v1_api = client.CoreV1Api()
         self.client_api = ApiClient()
 
-    def read(self, kind, name=None, namespace=None):
+    def read(self, kind, namespaced=True, name=None, namespace=None):
         if not namespace:
             namespace = self.active_namespace
-        method = "read_namespaced_{}".format(kind)
-        try:
-            c = self._client(kind)
-            result = getattr(c, method)(name=name, namespace=namespace)
-            return self.client_api.sanitize_for_serialization(result)
-        except AttributeError as e:
-            log.exception(e)
-            raise CommandExecutionError("Method {} not found".format(method))
-        except ApiException as e:
-            log.error("unable to fetch: {}".format(kind))
-            log.exception(e)
-            return None
+        method = "read_namespaced_{}".format(kind) if namespaced else "read_{}".format(kind)
+        return self._invoke(kind, method, name=name, namespace=namespace)
 
-    def list(self, kind, label_selector=None, namespace=None):
+    def list(self, kind, namespaced=True, label_selector=None, namespace=None):
         if not namespace:
             namespace = self.active_namespace
-        method = "list_namespaced_{}".format(kind)
+        method = "list_namespaced_{}".format(kind) if namespaced else "list_{}".format(kind)
+        return self._invoke(kind, method, label_selector=label_selector, namespace=namespace)
+
+    def _invoke(self, kind, method, **kwargs):
         try:
             c = self._client(kind)
-            result = getattr(c, method)(namespace=namespace, label_selector=label_selector)
+            result = getattr(c, method)(**kwargs)
             return self.client_api.sanitize_for_serialization(result)
         except AttributeError as e:
             log.exception(e)
@@ -71,7 +64,7 @@ class K8sClient(object):
             return None
 
     def _client(self, kind):
-        if kind == "pod":
+        if kind == "pod" or kind == "persistent_volume" or kind == "persistent_volume_claim":
             return self.client_core_v1_api
         else:
             return self.client_apps_v1_api
